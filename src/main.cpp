c945af0588c52d203e9abe579a54b538059ffc57
soraoo <zxc147895412@gmail.com>
2025-12-17 10:11:26 +0800

    调整下工程结构，依赖等

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
-#include "engine/app/app.h"
+#include <SDL3/SDL.h>
+#include <spdlog/spdlog.h>
 
 int main() {
-    engine::App app;
-    app.run();
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* _window = SDL_CreateWindow("anim", 1280, 720, 0);
+
+    spdlog::set_level(spdlog::level::info);
+    spdlog::info("Hello, world.");
+
+    auto is_running = true;
+    while (is_running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EventType::SDL_EVENT_QUIT) {
+                is_running = false;
+            }
+        }
+    }
+    
+    SDL_DestroyWindow(_window);
+    SDL_Quit();
+
     return 0;
 }
