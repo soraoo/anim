b2ba91c03f3556733034a7892522fbd3991d7064
soraoo <1142716989@qq.com>
2025-12-17 20:11:29 +0800

    再次调整，回归opengl

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,52 @@
 #include <SDL3/SDL.h>
 #include <spdlog/spdlog.h>
-#include <webgpu/webgpu_cpp.h>
+#include "glad/glad.h"
 
 int main() {
-    SDL_Init(SDL_INIT_VIDEO);
-
-    SDL_Window* _window = SDL_CreateWindow("anim", 1280, 720, 0);
-
-    spdlog::set_level(spdlog::level::info);
     spdlog::info("Hello, world.");
 
-    wgpu::InstanceDescriptor instance_desc{};
-    auto instance = wgpu::CreateInstance(&instance_desc);
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        return 1;
+    }
 
-    auto is_running = true;
-    while (is_running) {
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
+                        SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+
+    SDL_Window* _window = SDL_CreateWindow(
+        "anim", 1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+    if (!_window) {
+        return 1;
+    }
+
+    SDL_GLContext gl_context = SDL_GL_CreateContext(_window);
+    if (!gl_context) {
+        return 1;
+    }
+
+    SDL_GL_SetSwapInterval(1);
+
+    if (!gladLoadGLLoader(
+            reinterpret_cast<GLADloadproc>(SDL_GL_GetProcAddress))) {
+        return 1;
+    }
+
+    bool _is_running = true;
+    while (_is_running) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_EventType::SDL_EVENT_QUIT) {
-                is_running = false;
+                _is_running = false;
             }
         }
+
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        SDL_GL_SwapWindow(_window);
     }
-    
+
+    SDL_GL_DestroyContext(gl_context);
     SDL_DestroyWindow(_window);
     SDL_Quit();
 
